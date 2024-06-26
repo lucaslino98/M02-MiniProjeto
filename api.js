21cb98527c5cfb0cba7a8f36004a533cc5ea227d
Lucas Lino <lucaslino.martins@hotmail.com>
2024-06-26 16:30:11 -0300

    final

--- api.js
+++ api.js
@@ -1,10 +1,11 @@
-fetch('https://servicodados.ibge.gov.br/api/v3/noticias/?tipo=release', { method: 'GET' })
-    .then((retornoFetch) => {
-        return retornoFetch.json();
-    }).then((retornoApi) => {
 
-        const adcNoticias = document.querySelector('.title-news-today')
-        const titulo = retornoApi.items[0].titulo
-        const data = retornoApi.items[0].data_publicacao
-        adcNoticias.innerText = `${titulo} ${data}`
-    })
+async function noticiasDay() {
+    const retornoFetch = await fetch('https://servicodados.ibge.gov.br/api/v3/noticias/?tipo=release')
+    const retornoApi = await retornoFetch.json()
+    const adcNoticias = document.querySelector('.title-news-today')
+    const titulo = retornoApi.items[0].titulo
+    const data = retornoApi.items[0].data_publicacao
+    adcNoticias.innerText = `${titulo} ${data}`
+}
+
+noticiasDay()
--- index.js
+++ index.js
@@ -2,14 +2,35 @@ const listaInteresses = document.getElementById('lista-interesses')
 const inputInteresses = document.getElementById('inputADC')
 const buttonAdc = document.querySelector('.button-add')
 const inputADC = document.getElementById('inputADC')
+const buttonClear = document.querySelector('.button-clear')
+
+
+function fixarLocalStore() {
+    const declaraLocalStore = JSON.parse(localStorage.getItem('MinhaLista')) || [];
+    declaraLocalStore.forEach(item => {
+        const listaADC = document.createElement('li')
+        listaADC.innerHTML = item
+        listaInteresses.appendChild(listaADC)
+    })
+}
+fixarLocalStore()
+
 
 
 buttonAdc.addEventListener('click', () => {
-    const valueInput = inputADC.value
+    const valueInput = inputADC.value.trim()
     const criarLi = document.createElement('li')
+    const declaraLocalStore = JSON.parse(localStorage.getItem('MinhaLista')) || [];
+    declaraLocalStore.push(valueInput)
+    localStorage.setItem('MinhaLista', JSON.stringify(declaraLocalStore))
     criarLi.innerHTML = valueInput
-    listaInteresses.appendChild(criarLi)
+    listaInteresses.insertBefore(criarLi, listaInteresses.firstChild)
     inputADC.value = ''
 })
 
 
+buttonClear.addEventListener('click', () => {
+    localStorage.clear('MinhaLista')
+    const listaInteresses = document.getElementById('lista-interesses')
+    listaInteresses.innerHTML = ''
+})
